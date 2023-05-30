4dd10790dfd0c0c966f68bd406885f815c24ba0e
AyatoTT <Tyurin.vladislav2014@yandex.ru>
2023-05-31 02:17:30 +0300

    FF

---
+++ main.lua
@@ -0,0 +1,7 @@
+local composer = require("composer")
+
+-- загрузка первой сцены
+---composer.loadScene("game")
+--composer.loadScene("menu")
+
+composer.gotoScene("menu")
---
+++ menu.lua
@@ -0,0 +1,39 @@
+
+
+
+-- подключение библиотеки comp
+local composer = require("composer")
+
+-- создание сцены
+local scene = composer.newScene()
+
+-- функция обработки события нажатия на кнопку "Play"
+local function onPlayButtonTap(event)
+  if event.phase == "ended" then
+    composer.gotoScene("game") -- переход на сцену с игрой
+  end
+end
+
+-- функция отображения сцены
+function scene:create(event)
+  local sceneGroup = self.view
+
+  -- создание фона
+  local background = display.newRect(sceneGroup, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
+  background:setFillColor(0.1, 0.1, 0.1)
+
+  -- создание кнопки "Play"
+  local playButton = display.newText(sceneGroup, "Play", display.contentCenterX, display.contentCenterY, native.systemFont, 48)
+  playButton:setFillColor(1, 1, 1)
+  playButton:addEventListener("touch", onPlayButtonTap)
+
+  -- добавление объектов в сцену
+  sceneGroup:insert(background)
+  sceneGroup:insert(playButton)
+end
+
+-- настройка обработчиков событий
+scene:addEventListener("create", scene)
+
+return scene
+
