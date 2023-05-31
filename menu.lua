a6296443fe8a7828a6f6108114ccf937e063a683
AyatoTT <Tyurin.vladislav2014@yandex.ru>
2023-05-31 14:54:46 +0300

    FF

--- menu.lua
+++ menu.lua
@@ -22,14 +22,37 @@ function scene:create(event)
   local background = display.newRect(sceneGroup, display.contentCenterX, display.contentCenterY, display.contentWidth+200, display.contentHeight)
   background:setFillColor(0.1, 0.1, 0.1)
 
-  -- создание кнопки "Play"
-  local playButton = display.newText(sceneGroup, "Play", display.contentCenterX, display.contentCenterY, native.systemFont, 28)
-  playButton:setFillColor(0, 1, 1)
-  playButton:addEventListener("touch", onPlayButtonTap)
+
+
+
+
+  local playButton = display.newRect(sceneGroup, display.contentCenterX, display.contentCenterY, 200,300)
+  playButton:setFillColor(1, 1, 1)
+  playButton.cornerRadius = 10
+
+  local playButtonText = display.newText({
+    parent = sceneGroup,
+    text = "Play",
+    x = display.contentCenterX,
+    y = display.contentCenterY - 15,
+    font = native.systemFont,
+    fontSize = 24
+
+  })
+  playButtonText:setTextColor(1, 0, 0)
+
+  local logo = display.newImageRect("logo.png", 200,100)
+  logo.x = display.contentCenterX
+  logo.y = display.contentCenterY - 95
+
+
+  playButtonText:addEventListener("touch", onPlayButtonTap)
 
   -- добавление объектов в сцену
   sceneGroup:insert(background)
   sceneGroup:insert(playButton)
+  sceneGroup:insert(playButtonText)
+  sceneGroup:insert(logo)
 end
 
 -- настройка обработчиков событий
